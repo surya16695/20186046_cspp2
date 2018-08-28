37497f91f34e03433aec168da45181e99e33c777
Surya <suryasaginala@msitprogram.net>
2018-08-28 17:13:50 +0530

    first commit

---
+++ Mod_2/Assignment-1/Solution.java
@@ -0,0 +1,13 @@
+public class Solution {
+	/**
+	Program regarding the sy=tudent details.
+	@param args is parameter for this function. 
+	**/
+
+	public static void main(String[] args) {
+		/**
+		Taking input for details.
+		**/
+		System.out.println("Name :Surya , Roll Number : 20186046");
+	}
+}
