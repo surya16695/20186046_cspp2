0ec2a2b1b3769e0e343c15317b45f3c3149b6da5
Surya <suryasaginala@msitprogram.net>
2018-08-28 19:00:41 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- Mod_2/Assignment-3/Solution.java
+++ Mod_2/Assignment-3/Solution.java
@@ -1,15 +1,16 @@
 import java.util.Scanner;
 /* to find the given numbers power with an exp value*/
 public final class Solution {
-    /** To consider inputs**/
-    private Solution(){
-        /* not using the function*/
+    /** To consider inputs.**/
+    private Solution() {
+        /* not using the function.*/
     }
     /*
     Do not modify this main function.
     */
     public static void main(final String[] args) {
-        Scanner s = new Scanner(System.in);  
+        /** Assigns values.**/
+        Scanner s = new Scanner(System.in);
         int base = s.nextInt();
         int exponent = s.nextInt();
         long result = power(base, exponent);
@@ -19,7 +20,7 @@ public final class Solution {
     Need to write the power function and print the output.
     */
     public static long power(final int b, final int e) {
-        /** To evaluate the exponential function**/
+        /** To evaluate the exponential function.**/
         if (e > 0) {
             return b * (power(b, e - 1));
         } else {
