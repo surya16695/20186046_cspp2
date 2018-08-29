847e540e93ea02eae6766d568cb4b9dff0e90e8d
Surya <suryasaginala@msitprogram.net>
2018-08-29 09:33:17 +0530

    Factorial

---
+++ Mod_2/Factorial.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+/**
+ *@author SupriyaMadupathi
+ */
+public final class Factorial {
+    /*
+    *Do not modify this main function.
+    */
+    /**
+    * Empty Constructor.
+    */
+    private Factorial() {
+        //Iam not using this constructor
+    }
+    /**
+    * Main function to print.
+    * @param args is the parameter for this function
+    */
+    public static void main(final String[] args) {
+        Scanner s = new Scanner(System.in);
+        int number = s.nextInt();
+        int factorial = fact(number);
+        System.out.println(factorial);
+    }
+    /**
+    * { function_description }.
+    *
+    * @param n is the parameter of this method.
+    *
+    * @return we are returning n
+    */
+    public static int fact(final int num) {
+
+        if ((num == 0) || (num == 1)) {
+            return 1;
+        } else {
+            return num * fact(num - 1);
+        }
+    }
+}
