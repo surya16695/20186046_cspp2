32c7261124fd206f7f061837c60d071350e27310
Surya <suryasaginala@msitprogram.net>
2018-08-29 15:12:30 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2_assignments/mod_3/Assignment-3/Solution.java
+++ cspp2_assignments/mod_3/Assignment-3/Solution.java
@@ -18,7 +18,7 @@ public class Solution {
 	*/
     public static int gcd(int a, int b){
         int gcd = 0;      
-        for (int i = 0;  i <= a && i <= b; i++) {
+        for (int i = 1;  i <= a && i <= b; i++) {
             if ((a%i == 0) && (b%i == 0)){
                 gcd = i;
             }
