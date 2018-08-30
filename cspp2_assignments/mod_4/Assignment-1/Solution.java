3eba6fb8e1c31748928431c340893ca9fd571513
Surya <suryasaginala@msitprogram.net>
2018-08-30 15:42:01 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2_assignments/mod_4/Assignment-1/Solution.java
+++ cspp2_assignments/mod_4/Assignment-1/Solution.java
@@ -4,8 +4,7 @@ public class Solution {
     /*
     Fill this main function to print maximum of given array
     */
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] a = new int[n];
