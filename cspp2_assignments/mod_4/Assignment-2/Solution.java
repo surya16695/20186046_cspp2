18b09478583e1a28404d69a183c2090ab308f01e
Surya <suryasaginala@msitprogram.net>
2018-08-30 22:15:08 +0530

    "check_style_score -> 4 of 4 passed. style score: 1/1 "

--- cspp2_assignments/mod_4/Assignment-2/Solution.java
+++ cspp2_assignments/mod_4/Assignment-2/Solution.java
@@ -1,27 +1,42 @@
+/**
+*Add two matrices.
+**/
 import java.util.Scanner;
-import java.util.Arrays;
-public class Solution {
-    /* Fill the main function to print resultant of addition of matrices*/
-    public static void main(String[] args) {
+/**
+*used to take inputs.
+**/
+public final class Solution {
+    /**
+    *empty constructor.
+    **/
+    private Solution() {
+    //I am not using this function
+    }
+    /**
+    *main function.
+    *@param args is parameter for this function.
+    **/
+    public static void main(final String[] args) {
+        int m1, n1, m2, n2;
         Scanner s = new Scanner(System.in);
-        int m1 = s.nextInt();
-        int n1 = s.nextInt();
-        int m2 = s.nextInt();
-        int n2 = s.nextInt();
+        m1 = s.nextInt();
+        n1 = s.nextInt();
         int[][] a = new int[m1][n1];
-        for ( int i = 0; i < m1; i++) {
+        for (int i = 0; i < m1; i++) {
             for (int j = 0; j < n1; j++) {
                 a[i][j] = s.nextInt();
             }
         }
-        System.out.println(Arrays.toString(a));
+
+        m2 = s.nextInt();
+        n2 = s.nextInt();
         int[][] b = new int[m2][n2];
-        for ( int i = 0; i < m2; i++) {
+        for (int i = 0; i < m2; i++) {
             for (int j = 0; j < n2; j++) {
                 b[i][j] = s.nextInt();
             }
         }
-        System.out.println(Arrays.toString(b));
+
         int[][] c = new int[m2][n2];
         if (m1 == m2 && n1 == n2) {
             String res = "";
