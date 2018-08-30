f908e96c9bfdf39a58b07c9ad4cc49b09ca03582
Surya <suryasaginala@msitprogram.net>
2018-08-30 22:49:10 +0530

    ConverttoWhileLoop.java

---
+++ cspp2_practice/mod_3/ConverttoWhileLoop.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public final class ConverttoWhileLoop {
+	public static void main(final String[] args) {
+		Scanner s = new Scanner(System.in);
+		while (true) {
+			for (int i = 1; i < final 12; i++) {
+				if (i % 2 == 0) {
+				System.out.println(i);
+				}
+			}
+			break;
+		}
+		System.out.println("GOOD BYE!!");
+	}
+}
