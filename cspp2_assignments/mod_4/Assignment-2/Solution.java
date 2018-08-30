d9577197be05363803c161807b8d159ca0f281f9
Surya <suryasaginala@msitprogram.net>
2018-08-30 22:05:45 +0530

    "check_style_score -> 2 of 4 passed. style score: 0/1 "

--- cspp2_assignments/mod_4/Assignment-2/Solution.java
+++ cspp2_assignments/mod_4/Assignment-2/Solution.java
@@ -1,38 +1,37 @@
 import java.util.Scanner;
 public class Solution {
-	/* Fill the main function to print resultant of addition of matrices*/
-	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);
-		int m1 = s.nextInt();
-		int n1 = s.nextInt();
-		int m2 = s.nextInt();
-		int n2 = s.nextInt();
-		int[][] a = new int[m1][n1];
-		for ( int i = 0; i < m1; i++) {
-			for (int j = 0; j < n1; j++) {
-				a[i][j] = s.nextInt();
-			}
-		}
-		int[][] b = new int[m2][n2];
-		for ( int i = 0; i < m2; i++) {
-			for (int j = 0; j < n2; j++) {
-				b[i][j] = s.nextInt();
-			}
-		}
+    /* Fill the main function to print resultant of addition of matrices*/
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int m1 = s.nextInt();
+        int n1 = s.nextInt();
+        int m2 = s.nextInt();
+        int n2 = s.nextInt();
+        int[][] a = new int[m1][n1];
+        for ( int i = 0; i < m1; i++) {
+            for (int j = 0; j < n1; j++) {
+                a[i][j] = s.nextInt();
+            }
+        }
+        int[][] b = new int[m2][n2];
+        for ( int i = 0; i < m2; i++) {
+            for (int j = 0; j < n2; j++) {
+                b[i][j] = s.nextInt();
+            }
+        }
         int[][] c = new int[m2][n2];
-        if (m1 != m2 && n1 != n2) {
-            System.out.println("not possible");
-        } else {
+        if (m1 == m2 && n1 == n2) {
             String res = "";
-            for (int i = 0; i < m2; i++) {
+            for (int i = 0; i < m1; i++) {
                 for (int j = 0; j < n2; j++) {
                         c[i][j] = a[i][j] + b[i][j];
                         res += c[i][j] + " ";
                     }
                     res = res.trim() + "\n";
             }
-            System.out.println(res); 
-
+            System.out.print(res);
+        } else {
+            System.out.println("not possible");
         }
-	}
+    }
 }
