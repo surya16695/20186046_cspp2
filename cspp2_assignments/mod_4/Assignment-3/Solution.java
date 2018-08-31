e59600d7be3c96b72fe70e63e57f4652b15fa2b0
Surya <suryasaginala@msitprogram.net>
2018-08-31 13:37:56 +0530

    "check_style_score -> 0 of 1 passed. style score: 0/1 "

--- cspp2_assignments/mod_4/Assignment-3/Solution.java
+++ cspp2_assignments/mod_4/Assignment-3/Solution.java
@@ -19,9 +19,11 @@ public class Solution
 	}
 	public static int binaryToDecimal (String s) {
 		int sum = 0;
+		int k = 0;
 		for (int i = s.length() - 1; i >= 0; i--) {
 			if (s.charAt(i) == '1') {
-				sum += Math.pow(2, i);
+				sum += Math.pow(2, k);
+				k += 2;
 				// System.out.println(sum);
 			}
 		}
