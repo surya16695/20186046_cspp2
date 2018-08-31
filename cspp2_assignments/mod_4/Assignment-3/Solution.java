3ff2371672a54276ce241631e776925186eb7bca
Surya <suryasaginala@msitprogram.net>
2018-08-31 14:13:24 +0530

    "check_style_score -> 1 of 1 passed. style score: 0/1 "

--- cspp2_assignments/mod_4/Assignment-3/Solution.java
+++ cspp2_assignments/mod_4/Assignment-3/Solution.java
@@ -10,18 +10,19 @@ public class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        for(int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             String s = sc.next();
-            int res = binaryToDecimal(s);//Write binaryToDecimal function
+            int res = binaryToDecimal(s);
+            /**Write binaryToDecimal function.**/
             System.out.println(res);
         }
     }
     public static int binaryToDecimal (String s) {
         int sum = 0;
-        for(int i = 0; i < s.length(); i++) {
+        for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) == '1') {
                 sum += Math.pow(2, s.length() - i - 1);
-                // System.out.println(sum);
+                /**System.out.println(sum).**/
             }
         }
         return (sum);
