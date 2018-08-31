f884f05a0807d017d323f41e49d9e8a3593dc349
Surya <suryasaginala@msitprogram.net>
2018-08-31 17:46:23 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2_assignments/mod_4/Assignment-4/Solution.java
+++ cspp2_assignments/mod_4/Assignment-4/Solution.java
@@ -5,28 +5,38 @@ import java.util.Scanner;
 
 import java.lang.String;
 
+/**
+ * Class for solution.
+ */
 public class Solution {
-	/*
-	Do not modify the main function. 
-	*/
-	/**
+    /*
+    Do not modify the main function.
+    */
+    /**
     @param s Do not modify this main function.
     @return integer value.
     */
-	public static void main(final String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String s = sc.nextLine();
-		String reverse = reverse(s);
-		System.out.println(reverse);
-	}
-	/**Write reverseString function.**/
-	public static String reverse(final String s) {
-    	/**@param s Do not modify this main function.**/
-		String rev = "";
-		for (int i = s.length() - 1; i >= 0; i--) {
-			rev = rev + (s .charAt(i));
-		}
-		return rev;
-	}
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        String reverse = reverse(s);
+        System.out.println(reverse);
+    }
+    /**Write reverseString function.**/
+    /**
+     * { function_description }
+     *
+     * @param      s     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public static String reverse(final String s) {
+        /**@param s Do not modify this main function.**/
+        String rev = "";
+        for (int i = s.length() - 1; i >= 0; i--) {
+            rev = rev + (s .charAt(i));
+        }
+        return rev;
+    }
 
 }
