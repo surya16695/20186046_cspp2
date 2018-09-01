d6903be8f6234ad132bfaae16dff22bb7b2dbf51
Surya <suryasaginala@msitprogram.net>
2018-09-01 10:07:33 +0530

    "check_style_score -> 6 of 6 passed. style score: 0/1 "

--- cspp2_assignments/mod_6/Assignment-1/Solution.java
+++ cspp2_assignments/mod_6/Assignment-1/Solution.java
@@ -1,9 +1,10 @@
+
 import java.util.Scanner;
 /**
  * Write a java program to find the odd composite numbers
  * between 2 and n where n is given as an input.
  *
- * @author : 
+ * @author :
  */
 public final class Solution {
     /**
@@ -12,24 +13,32 @@ public final class Solution {
     private Solution() {
         //not used
     }
+
     /**
-     * Prints the odd composite numbers between 2 and n.
+     * Determines if prime.
      *
-     * @param      n     n value
+     * @param      n     { parameter_description }
+     *
+     * @return     True if prime, False otherwise.
      */
-    public static boolean isPrime(int n) {
+    public static boolean isPrime(final int n) {
         boolean flag = false;
-        for(int i = 2; i <= n/2; ++i) {
+        for (int i = 2; i <= n / 2; i++) {
             // condition for nonprime number
-            if(n % i == 0) {
+            if (n % i == 0) {
                 flag = true;
                 break;
             }
         }
         return flag;
     }
+    /**
+     * { printing composite numbers }
+     *
+     * @param      n     { parameter_description }
+     */
     static void oddComposites(final int n) {
-	// write your code here
+    // write your code here
         for (int i = 2; i < n; i++) {
             if (i % 2 != 0) {
                 if (isPrime(i) == true) {
