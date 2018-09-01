3208deb2574d221ff186638d58a4e2e08cf2aa3c
Surya <suryasaginala@msitprogram.net>
2018-09-01 11:07:18 +0530

    "check_style_score -> 6 of 6 passed. style score: 0/1 "

--- cspp2_assignments/mod_6/Assignment-1/Solution.java
+++ cspp2_assignments/mod_6/Assignment-1/Solution.java
@@ -42,7 +42,7 @@ public final class Solution {
         for (int i = 2; i < n; i++) {
             if (i % 2 != 0) {
                 isPrime(i);
-                if (isPrime(i) == true) {
+                if (isPrime(i)) {
                     System.out.println(i);
                 }
             }
