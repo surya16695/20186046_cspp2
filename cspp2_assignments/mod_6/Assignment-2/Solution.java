3ae66425f29daef4aad2964ce917e206408a9da9
Surya <suryasaginala@msitprogram.net>
2018-09-01 10:44:31 +0530

    "check_style_score -> 0 of 5 passed. style score: 0/1 "

---
+++ cspp2_assignments/mod_6/Assignment-2/Solution.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+/**
+ * Write a java program to round the
+ * elements of a matrix to the nearest 100.
+ *
+ * @author : 
+ */
+final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //not used
+    }
+    /**
+     * Function to round the
+     * elements of a matrix to the nearest 100.
+     *
+     * @param      a     Matrix of which the elements to be rounded
+     * @param      rows     Number of rows
+     * @param      columns     Number of columns
+     *
+     * @return     Matrix of the rounded elements
+     */
+    static double[][] roundHundred(final int[][] a, final int rows, final int columns) {
+
+	// write ypur code here
+        double[][] c = new double[rows][columns];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                double d = (a[i][j]/100);
+                d = Math.floor(d);
+                System.out.println(d);
+                if (c[i][j] > 0) {
+                    c[i][j] = d *100;
+                }
+            }  
+        }
+        return c;
+    }
+    /**
+     * Main function.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int m = scan.nextInt();
+        int n = scan.nextInt();
+        int[][] a = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                a[i][j] = scan.nextInt();
+            }
+        }
+        double[][] b = roundHundred(a, m, n);
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n - 1; j++) {
+                System.out.print(b[i][j] + " ");
+            }
+            System.out.println(b[i][n - 1]);
+        }
+    }
+}
