dc75dc4584a15c87c6681e7eefbc0ea3b5815e9b
Surya <suryasaginala@msitprogram.net>
2018-09-03 20:42:11 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2_assignments/mod_7/Assignment-1/Solution.java
+++ cspp2_assignments/mod_7/Assignment-1/Solution.java
@@ -1,28 +1,42 @@
 import java.util.*;
 class InputValidator
 {
-	String data1;
-	InputValidator(String data) {
-		this.data1 = data;
-	}
-	public  boolean validateData()
-	{
-		return (this.data1.length() >= 6);
-	}
-	/*Write the atrributes and methods for InputValidator*/
+    /**
+     * { var_description }
+     */
+    String data1;
+    InputValidator(String data) {
+        this.data1 = data;
+    }
+    /**
+     * { function_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public  boolean validateData()
+    {
+        return (this.data1.length() >= 6);
+    }
+    /*Write the atrributes and methods for InputValidator*/
 }
+/**
+ * Class for solution.
+ */
 public class Solution
 {
-	private Solution() {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
 
-	}
+    }
 
-	public static void main(String args[])
+    public static void main(String args[])
     {
-    	Scanner s=new Scanner(System.in);
-    	String input=s.next();
-    	InputValidator i=new InputValidator(input);    	
-    	System.out.println(i.validateData());
+        Scanner s = new Scanner(System.in);
+        String input = s.next();
+        InputValidator i = new InputValidator(input);     
+        System.out.println(i.validateData());
 
     }
 
