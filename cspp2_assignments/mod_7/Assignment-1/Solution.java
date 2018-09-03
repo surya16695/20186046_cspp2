e1249fa5fd760b07a5794d9813bfbf07cfd98d2a
Surya <suryasaginala@msitprogram.net>
2018-09-03 21:40:55 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2_assignments/mod_7/Assignment-1/Solution.java
+++ cspp2_assignments/mod_7/Assignment-1/Solution.java
@@ -44,7 +44,7 @@ public final class Solution {
      */
     public static void main(final String[] args) {
         Scanner s = new Scanner(System.in);
-        String input = s.next();
+        String input = s.nextLine();
         InputValidator i = new InputValidator(input);
         System.out.println(i.validateData());
 
