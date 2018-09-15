ca885283f88b05bf8206bc52f410d432e06e582f
Surya <suryasaginala@msitprogram.net>
2018-09-15 11:08:03 +0530

    "check_style_score -> 0 of 13 passed. style score: 0/1 "

---
+++ cspp2_assignments/mod_19/Assignment-1/Solution.java
@@ -0,0 +1,114 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+class Quiz {
+    private String [] qu;
+    private int size;
+    protected Quiz() {
+        String[] qu = new String[10];
+        int size = 0;  
+    }
+}
+/**
+ * Solution class for code-eval.
+ */
+public final class Solution {
+     /**
+     * Constructs the object.
+     */
+    private Solution() {
+        // leave this blank
+    }
+    /**
+     * main function to execute test cases.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+         // instantiate this Quiz
+        Quiz q = new Quiz();
+         // code to read the test cases input file
+        Scanner s = new Scanner(System.in);
+        // check if there is one more line to process
+        while (s.hasNext()) {
+            // read the line
+            String line = s.nextLine();
+             // split the line using space
+            String[] tokens = line.split(" ");
+              // based on the list operation invoke the corresponding method
+            switch (tokens[0]) {
+                case "LOAD_QUESTIONS":
+                System.out.println("|----------------|");
+                System.out.println("| Load Questions |");
+                System.out.println("|----------------|");
+                loadQuestions(s, q, Integer.parseInt(tokens[1]));
+                break;
+                case "START_QUIZ":
+                System.out.println("|------------|");
+                System.out.println("| Start Quiz |");
+                System.out.println("|------------|");
+                startQuiz(s, q, Integer.parseInt(tokens[1]));
+                break;
+                case "SCORE_REPORT":
+                System.out.println("|--------------|");
+                System.out.println("| Score Report |");
+                System.out.println("|--------------|");
+                displayScore(q);
+                break;
+                default:
+                break;
+            }
+        }
+    }
+    // public static void add(String[] tokens) {
+    //     String[] qu = new String[10];
+    //     int size = 0;
+    //     qu[size++] = tokens;
+    // }
+    /**
+     * Loads questions.
+     *
+     * @param      s              The scanner object for user input
+     * @param      quiz           The quiz object
+     * @param      questionCount  The question count
+     */
+    public static void loadQuestions(final Scanner s, final Quiz quiz, final int questionCount) {
+        // write your code here to read the questions from the console
+        // tokenize the question line and create the question object
+        // add the question objects to the quiz class.
+        String line = s.nextLine();
+        String[] qu = new String[10];
+        System.out.println(questionCount+"are added to the quiz");
+        for (int i = 0; i < questionCount; i++) {
+            String[] tokens = line.split(":");
+            qu[i] += tokens; 
+            System.out.println(Arrays.toString(tokens));
+            // String[] question = tokens[0];
+            // String[] choice = tokens[1];
+        }
+    }
+            
+
+
+    /**
+     * Starts a quiz.
+     *
+     * @param      s            The scanner object for user input
+     * @param      quiz         The quiz object
+     * @param      answerCount  The answer count
+     */
+    public static void startQuiz(final Scanner s, final Quiz quiz, final int answerCount) {
+        // write your code here to display the quiz questions
+        // read the user responses from the console
+        // store the user respones in the quiz object
+    }
+
+    /**
+     * Displays the score report
+     *
+     * @param      quiz     The quiz object
+     */
+    public static void displayScore(final Quiz quiz) {
+        // write your code here to display the score report
+    }
+}
